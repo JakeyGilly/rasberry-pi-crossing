12b563d3207409117de2567eec6a66639b502d15
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2020-02-26 15:27:51 +0000

    added traffic lights

--- main.py
+++ main.py
@@ -1,21 +1,64 @@
+import tkinter
 import face_recognition
 import cv2
+import _thread
+import time
+from PIL import Image, ImageTk
 video_capture = cv2.VideoCapture(0)
 face_locations = []
-face_names = []
+frame_counter = 0
+root = tkinter.Tk()
+root.configure(background='green')
+root.update()
 while True:
     ret, frame = video_capture.read()
     rgb_frame = frame[:, :, ::-1]
     face_locations = face_recognition.face_locations(rgb_frame)
-    name = "Person"
-    face_names.append(name)
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
+    print(face_locations)
+    for (top, right, bottom, left) in face_locations:
         cv2.rectangle(frame, (left, top), (right, bottom), (60,60,60), 2)
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (60,60,60), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (0, 0, 0), 2)
+        if len(face_locations) != 0:
+            frame_counter += 1
+            if frame_counter == 15:
+                print("Face Detected and waiting")
+                frame_counter = 0
+                time.sleep(3)
+                ret, frame = video_capture.read()
+                rgb_frame = frame[:, :, ::-1]
+                face_locations = face_recognition.face_locations(rgb_frame)
+                if len(face_locations) != 0:
+                    root.configure(background='yellow')
+                    root.update()
+                    time.sleep(1)
+                    root.configure(background='red')
+                    root.update()
+                    while len(face_locations) != 0:
+                        video_capture = cv2.VideoCapture(0)
+                        ret, frame = video_capture.read()
+                        rgb_frame = frame[:, :, ::-1]
+                        face_locations = face_recognition.face_locations(rgb_frame)
+                        print(face_locations)
+                    time.sleep(5)
+                    root.configure(background='yellow')
+                    root.update()
+                    time.sleep(2)
+                    root.configure(background='green')
+                    root.update()
+                    
+                    
+                    
     cv2.imshow('Face Recognition', frame)
     if cv2.waitKey(1) == 27:
         break
 video_capture.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows
+
+
+# SMART CROSSING
+# 1. Detect if phone is near the rasberry pi (or with face recognition for abled people)
+
+# 2. send a signal to phone to alert the crossing is near
+
+# 3. if in range while green man send notification to phone/watch (2 buzzes)
+
+# 4. cross
